64ce325dde582c43f5db35bfbcc8fc14e84b3aa0
Hyeonseop-Noh <heonsub6558@gmail.com>
2022-04-01 18:12:51 +0900

    Remove logger

--- src/main/java/com/cos/photogram/web/AuthController.java
+++ src/main/java/com/cos/photogram/web/AuthController.java
@@ -1,17 +1,14 @@
 package com.cos.photogram.web;
 
 import com.cos.photogram.web.dto.auth.SignupDto;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.logging.Logger;
 
 @Controller
 public class AuthController {
 
-    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
 
     @GetMapping("/auth/signin")
     public String signinForm() {
@@ -26,7 +23,7 @@ public class AuthController {
     @PostMapping("/auth/signup")
     public String signup(SignupDto signupDto) {
 
-        log.info(signupDto.toString());
+
         return "auth/signin";
     }
 
