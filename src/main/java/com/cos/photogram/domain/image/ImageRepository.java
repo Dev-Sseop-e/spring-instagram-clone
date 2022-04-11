e4e910bd80b711703533a28f15358119516b0e90
Hyeonseop-Noh <heonsub6558@gmail.com>
2022-04-11 09:44:06 +0900

    fix typo

--- src/main/java/com/cos/photogram/domain/image/ImageRepository.java
+++ src/main/java/com/cos/photogram/domain/image/ImageRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Integer> {
 
-    @Query(value = "SELECT * FROM image WHERE userId IN (SELECT toUserId FROM subscribe WHERE fronUserId = :principalId) ORDER BY id DESC", nativeQuery = true)
+    @Query(value = "SELECT * FROM image WHERE userId IN (SELECT toUserId FROM subscribe WHERE fromUserId = :principalId) ORDER BY id DESC", nativeQuery = true)
     Page<Image> mStory(int principalId, Pageable pageable);
 
 }
